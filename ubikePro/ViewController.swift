9988e6406b3ecd923bd0974b24718907e46f4d3f
jemiway <jemiway0816@gmail.com>
2024-06-17 15:43:01 +0800

    Initial Commit

---
+++ ubikePro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ubikePro
+//
+//  Created by CHUN-CHIEH LU on 2024/6/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
